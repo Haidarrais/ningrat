ce2e70d451697d307c514f9ab8833f59fa0584ff
Haidar Rais <abdillah.fams@gmail.com>
2021-10-04 15:57:38 +0700

    royalty

--- resources/views/partials/dashboard/sidebar.blade.php
+++ resources/views/partials/dashboard/sidebar.blade.php
@@ -43,6 +43,7 @@
                     <li><a class="nav-link" href="{{ route('category.index') }}">Kategori</a></li>
                     <li><a class="nav-link" href="{{ route('product.index') }}">Produk</a></li>
                     <li><a class="nav-link" href="{{ route('discount.index') }}">Diskon</a></li>
+                    <li><a class="nav-link" href="{{ route('royalty.index') }}">Royalty</a></li>
                 </ul>
             </li>
             @endrole
@@ -57,7 +58,6 @@
                 <ul class="dropdown-menu">
                     <li><a class="nav-link" href="{{ route('users.index') }}">Users</a></li>
                     <li><a class="nav-link" href="{{ route('courier.index') }}">Courier</a></li>
-                    <li><a class="nav-link" href="{{ route('royalty.index') }}">Royalty</a></li>
                     <li><a class="nav-link" href="{{ route('point.index') }}">Point</a></li>
                     <li><a class="nav-link" href="{{ route('reward.index') }}">Reward</a></li>
                     <li><a class="nav-link" href="{{ route('setting.index') }}">Pengaturan</a></li>
