22f3032a7cecf2936e4bb8260de465df7db0c165
Kacong Arip <arifsaqa@gmail.com>
2021-10-04 17:02:44 +0700

    todays month orders query

--- app/Http/Controllers/Web/OrderController.php
+++ app/Http/Controllers/Web/OrderController.php
@@ -401,7 +401,7 @@ class OrderController extends Controller
         $orders =  Order::select(
             DB::raw('sum(subtotal) as sums'),
             DB::raw("DATE_FORMAT(created_at,'%m') as month")
-        )->whereIn('user_id', $hirarki)
+        )
             ->orWhere('user_id', $user->id)
             ->where('status', 4)
             ->whereMonth('created_at', $todaysMonth)
