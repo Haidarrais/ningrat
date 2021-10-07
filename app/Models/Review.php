407c27a200f93455c9b61420cfb7e29d16d01a7d
Haidar Rais <abdillah.fams@gmail.com>
2021-10-07 16:05:45 +0700

    review

---
+++ app/Models/Review.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'transaction_id',
+        'member_id',
+        'buyer_id',
+        'rating'
+    ];
+
+    public function transaction(){
+        $this->belongsTo(Transaction::class);
+    }
+    public function member(){
+        $this->belongsTo(Member::class);
+    }
+    public function buyer(){
+        $this->belongsTo(Member::class, 'buyer_id');
+    }
+}
