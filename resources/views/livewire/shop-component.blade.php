9683ba640cac44a4182c8a02bdcfc144076338cd
Haidar Rais <abdillah.fams@gmail.com>
2021-10-11 20:18:02 +0700

    cart

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -251,11 +251,14 @@
                     url: url,
                     success: function(data) {
                         data.data.forEach(item => {
-                                document.getElementById("myTabContent").innerHTML += '<div class="tab-pane fade show active" id="single-slide" role="tabpanel" aria-labelledby="single-slide-tab-"><div class="single-product-img"><img src="{{ URL::asset("upload/product/") }}'+item.image+'"></div></div>';
-                                document.getElementById("myTabPanel").innerHTML += '<div class="nav single-slide-menu" role="tablist"><div class="single-tab-menu"><a class="active" data-toggle="tab" id="single-slide-tab-" href="#single-slide"><img src="{{ URL::asset("upload/product/") }}'+item.image+'" alt="" ></a></div></div>';
+                            if (index = 0) {
+                                document.getElementById("myTabContent").innerHTML += '<div class="tab-pane fade show active" id="single-slide'+ index+1 +'" role="tabpanel" aria-labelledby="single-slide-tab-'+ index+1 +'"><div class="single-product-img"><img src="#"></div></div>';
+                                document.getElementById("myTabPanel").innerHTML += '<div class="nav single-slide-menu" role="tablist"><div class="single-tab-menu"><a class="active" data-toggle="tab" id="single-slide-tab-'+ index+1 +'" href="#single-slide'+ index+1 +'"><img src="#" alt="" ></a></div></div>';
+                            }else{
+                                document.getElementById("myTabContent").innerHTML += '<div class="tab-pane fade" id="single-slide'+ index+1 +'" role="tabpanel" aria-labelledby="single-slide-tab-'+ index+1 +'"><div class="single-product-img"><img src="#"></div></div>';
+                                document.getElementById("myTabPanel").innerHTML += '<div class="nav single-slide-menu" role="tablist"><div class="single-tab-menu"><a data-toggle="tab" id="single-slide-tab-'+ index+1 +'" href="#single-slide'+ index+1 +'"><img src="#" alt="" ></a></div></div>';
                             }
                         });
-
                     },
                 });
             }
