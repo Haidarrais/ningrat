4095a7aab26840f5f863420cbd35f29d6fb88c49
Haidar Rais <abdillah.fams@gmail.com>
2021-10-12 00:53:15 +0700

    cart

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -264,6 +264,15 @@
                                 htmlL += `<div class="single-tab-menu">
                                             <a class="active" data-toggle="tab" id="single-slide-tab-${key+1}"" href="#single-slide${key+1}""><img src="{{ asset('upload/product/${value.image}') }}" alt="" ></a>
                                         </div>`
+                            }else{
+                                htmlC += `<div class="tab-pane fade" id="single-slide${key+1}" role="tabpanel" aria-labelledby="single-slide-tab-${key+1}">
+                                            <div class="single-product-img">
+                                                <img src="{{ asset('upload/product/${value.image}') }}" alt="tidak ada gambar">
+                                            </div>
+                                        </div>`
+                                htmlL += `<div class="single-tab-menu">
+                                            <a data-toggle="tab" id="single-slide-tab-${key+1}"" href="#single-slide${key+1}""><img src="{{ asset('upload/product/${value.image}') }}" alt="" ></a>
+                                        </div>`
                             }
                         });
                         $(`#myTabList${id}`).html(htmlL)
