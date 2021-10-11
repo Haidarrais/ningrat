47aa23daef080f14f3d12671b9045c5b6c42317e
Haidar Rais <abdillah.fams@gmail.com>
2021-10-11 19:46:05 +0700

    cart

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -254,15 +254,15 @@
                         data.data.forEach(sendData);
                         console.log(sendData);
 
-                        function sendData(item, index) {
-                            if (index = 0) {
-                                tabContent += "<div class='tab-pane fade show active' id='single-slide"+ index+1 +"' role='tabpanel' aria-labelledby='single-slide-tab-"+ index+1 +"'>" + "<div class='single-product-img'>" + "<img src='{{ asset('upload/product/'.'"+ item.image +"')}}'>" + "</div></div>";
-                                tabPanel += "<div class='nav single-slide-menu' role='tablist'><div class='single-tab-menu'><a class='active' data-toggle='tab' id='single-slide-tab-"+ index+1 +"' href='#single-slide"+ index+1 +"'><img src='{{ asset('upload/product/'.'"+ item.image +"') }}' alt='' ></a></div></div>";
-                            }else{
-                                tabContent += "<div class='tab-pane fade' id='single-slide"+ index+1 +"' role='tabpanel' aria-labelledby='single-slide-tab-"+ index+1 +"'>" + "<div class='single-product-img'>" + "<img src='{{ asset('upload/product/'.'"+ item.image +"')}}'>" + "</div></div>";
-                                tabPanel += "<div class='nav single-slide-menu' role='tablist'><div class='single-tab-menu'><a data-toggle='tab' id='single-slide-tab-"+ index+1 +"' href='#single-slide"+ index+1 +"'><img src='{{ asset('upload/product/'.'"+ item.image +"') }}' alt='' ></a></div></div>";
-                            }
-                        }
+                        // function sendData(item, index) {
+                        //     if (index = 0) {
+                        //         tabContent += "<div class='tab-pane fade show active' id='single-slide"+ index+1 +"' role='tabpanel' aria-labelledby='single-slide-tab-"+ index+1 +"'>" + "<div class='single-product-img'>" + "<img src='{{ asset('upload/product/'.'"+ item.image +"')}}'>" + "</div></div>";
+                        //         tabPanel += "<div class='nav single-slide-menu' role='tablist'><div class='single-tab-menu'><a class='active' data-toggle='tab' id='single-slide-tab-"+ index+1 +"' href='#single-slide"+ index+1 +"'><img src='{{ asset('upload/product/'.'"+ item.image +"') }}' alt='' ></a></div></div>";
+                        //     }else{
+                        //         tabContent += "<div class='tab-pane fade' id='single-slide"+ index+1 +"' role='tabpanel' aria-labelledby='single-slide-tab-"+ index+1 +"'>" + "<div class='single-product-img'>" + "<img src='{{ asset('upload/product/'.'"+ item.image +"')}}'>" + "</div></div>";
+                        //         tabPanel += "<div class='nav single-slide-menu' role='tablist'><div class='single-tab-menu'><a data-toggle='tab' id='single-slide-tab-"+ index+1 +"' href='#single-slide"+ index+1 +"'><img src='{{ asset('upload/product/'.'"+ item.image +"') }}' alt='' ></a></div></div>";
+                        //     }
+                        // }
                     },
                 });
             }
