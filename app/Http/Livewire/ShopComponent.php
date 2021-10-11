31f5e7ac2d77865483266db07864fce4ba2f6c8f
Haidar Rais <abdillah.fams@gmail.com>
2021-10-11 18:04:09 +0700

    cart

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -140,7 +140,7 @@ class ShopComponent extends Component
             }
         }
         $pictures = ProductPicture::where('product_id', '=', $this->pictureId)->get();
-        $this->pictures = $pictures ?? false;
+        $this->pictures = $pictures;
         $this->categories = Category::all();
         return view('livewire.shop-component',['stocks' => $stocks])->layout('layouts.main');
     }
