af0a2bab53987c37f40b40275fa048db7c9759ed
Haidar Rais <abdillah.fams@gmail.com>
2021-10-11 20:06:56 +0700

    cart

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -253,13 +253,8 @@
                         data.data.forEach(sendData);
                         console.log(sendData);
                         function sendData(item, index) {
-                            if (index = 0) {
-                                document.getElementById("myTabContent").innerHTML += '<div class="tab-pane fade show active" id="single-slide'+ index+1 +'" role="tabpanel" aria-labelledby="single-slide-tab-'+ index+1 +'"><div class="single-product-img"><img src="{{ asset("upload/product/'+ item.image +'")}}"></div></div>';
-                                document.getElementById("myTabPanel").innerHTML += '<div class="nav single-slide-menu" role="tablist"><div class="single-tab-menu"><a class="active" data-toggle="tab" id="single-slide-tab-'+ index+1 +'" href="#single-slide'+ index+1 +'"><img src="{{ asset("upload/product/'+ item.image +'") }}" alt="" ></a></div></div>';
-                            }else{
-                                document.getElementById("myTabContent").innerHTML += '<div class="tab-pane fade" id="single-slide'+ index+1 +'" role="tabpanel" aria-labelledby="single-slide-tab-'+ index+1 +'"><div class="single-product-img"><img src="{{ asset("upload/product/'+ item.image +'")}}"></div></div>';
-                                document.getElementById("myTabPanel").innerHTML += document.getElementById("myTabPanel").innerHTML += '<div class="nav single-slide-menu" role="tablist"><div class="single-tab-menu"><a data-toggle="tab" id="single-slide-tab-'+ index+1 +'" href="#single-slide'+ index+1 +'"><img src="{{ asset("upload/product/'+ item.image +'") }}" alt="" ></a></div></div>';
-                            }
+                            document.getElementById("myTabContent").innerHTML += '<div class="tab-pane fade show active" id="single-slide'+ index+1 +'" role="tabpanel" aria-labelledby="single-slide-tab-'+ index+1 +'"><div class="single-product-img"><img src="{{ asset("upload/product/'+ item.image +'")}}"></div></div>';
+                            document.getElementById("myTabPanel").innerHTML += '<div class="nav single-slide-menu" role="tablist"><div class="single-tab-menu"><a class="active" data-toggle="tab" id="single-slide-tab-'+ index+1 +'" href="#single-slide'+ index+1 +'"><img src="{{ asset("upload/product/'+ item.image +'") }}" alt="" ></a></div></div>';
                         }
                     },
                 });
