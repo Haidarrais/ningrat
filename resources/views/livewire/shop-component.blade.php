2ce9fc67f33cf9e0c6f417f460ffec0f5284068b
Haidar Rais <abdillah.fams@gmail.com>
2021-10-12 00:25:55 +0700

    cart

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -251,21 +251,21 @@
                     type: 'GET',
                     url: url,
                     success: function(data) {
-                        console.log(data);
                         let picture = data.data
                         let htmlC = ``
                         let htmlL = ``
                         $.each(picture, (key, value) => {
                             if (key == 0) {
-                                htmlC = `<div class="tab-pane fade show active" id="single-slide${key+1}" role="tabpanel" aria-labelledby="single-slide-tab-${key+1}">
+                                htmlC += `<div class="tab-pane fade show active" id="single-slide${key+1}" role="tabpanel" aria-labelledby="single-slide-tab-${key+1}">
                                             <div class="single-product-img">
                                                 <img src="{{ asset('upload/product/${value.image}') }}" alt="tidak ada gambar">
                                             </div>
                                         </div>`
-                                htmlL = `<div class="single-tab-menu">
+                                htmlL += `<div class="single-tab-menu">
                                             <a class="active" data-toggle="tab" id="single-slide-tab-${key+1}"" href="#single-slide${key+1}""><img src="{{ asset('upload/product/${value.image}') }}" alt="" ></a>
                                         </div>`
                             }
+                            console.log(htmlL);
                             $("#myTabContent").html(htmlC)
                             $("#myTabList").html(htmlL)
                         });
