3fd58aec8993d71a58bacd5bb1d065a260c20a97
Haidar Rais <abdillah.fams@gmail.com>
2021-10-11 19:52:16 +0700

    cart

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -253,7 +253,7 @@
                         console.log(data);
                         data.data.forEach(sendData);
                         function sendData(item, index) {
-                        console.log(item);
+                        console.log(item.image);
 
                             // if (index = 0) {
                             //     document.getElementById("myTabContent").innerHTML += "<div class='tab-pane fade show active' id='single-slide"+ index+1 +"' role='tabpanel' aria-labelledby='single-slide-tab-"+ index+1 +"'>" + "<div class='single-product-img'>" + "<img src='{{ asset('upload/product/'.'"+ item.image +"')}}'>" + "</div></div>";
