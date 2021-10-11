d5b13b2fa7ef88503d40777c93a69d40d149a2cd
Haidar Rais <abdillah.fams@gmail.com>
2021-10-11 19:50:13 +0700

    cart

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -246,8 +246,6 @@
                 // console.log(index);
                 var url = "{{route('picture.show', ":id")}}";
                 url = url.replace(":id", id);
-                document.getElementById("myTabContent").innerHTML = tabContent;
-                document.getElementById("myTabPanel").innerHTML = tabPanel;
                 $.ajax({
                     type: 'GET',
                     url: url,
@@ -256,11 +254,11 @@
                         data.data.forEach(sendData);
                         function sendData(item, index) {
                             if (index = 0) {
-                                tabContent += "<div class='tab-pane fade show active' id='single-slide"+ index+1 +"' role='tabpanel' aria-labelledby='single-slide-tab-"+ index+1 +"'>" + "<div class='single-product-img'>" + "<img src='{{ asset('upload/product/'.'"+ item.image +"')}}'>" + "</div></div>";
-                                tabPanel += "<div class='nav single-slide-menu' role='tablist'><div class='single-tab-menu'><a class='active' data-toggle='tab' id='single-slide-tab-"+ index+1 +"' href='#single-slide"+ index+1 +"'><img src='{{ asset('upload/product/'.'"+ item.image +"') }}' alt='' ></a></div></div>";
+                                document.getElementById("myTabContent").innerHTML += "<div class='tab-pane fade show active' id='single-slide"+ index+1 +"' role='tabpanel' aria-labelledby='single-slide-tab-"+ index+1 +"'>" + "<div class='single-product-img'>" + "<img src='{{ asset('upload/product/'.'"+ item.image +"')}}'>" + "</div></div>";
+                                document.getElementById("myTabPanel").innerHTML += "<div class='nav single-slide-menu' role='tablist'><div class='single-tab-menu'><a class='active' data-toggle='tab' id='single-slide-tab-"+ index+1 +"' href='#single-slide"+ index+1 +"'><img src='{{ asset('upload/product/'.'"+ item.image +"') }}' alt='' ></a></div></div>";
                             }else{
-                                tabContent += "<div class='tab-pane fade' id='single-slide"+ index+1 +"' role='tabpanel' aria-labelledby='single-slide-tab-"+ index+1 +"'>" + "<div class='single-product-img'>" + "<img src='{{ asset('upload/product/'.'"+ item.image +"')}}'>" + "</div></div>";
-                                tabPanel += "<div class='nav single-slide-menu' role='tablist'><div class='single-tab-menu'><a data-toggle='tab' id='single-slide-tab-"+ index+1 +"' href='#single-slide"+ index+1 +"'><img src='{{ asset('upload/product/'.'"+ item.image +"') }}' alt='' ></a></div></div>";
+                                document.getElementById("myTabContent").innerHTML += "<div class='tab-pane fade' id='single-slide"+ index+1 +"' role='tabpanel' aria-labelledby='single-slide-tab-"+ index+1 +"'>" + "<div class='single-product-img'>" + "<img src='{{ asset('upload/product/'.'"+ item.image +"')}}'>" + "</div></div>";
+                                document.getElementById("myTabPanel").innerHTML += "<div class='nav single-slide-menu' role='tablist'><div class='single-tab-menu'><a data-toggle='tab' id='single-slide-tab-"+ index+1 +"' href='#single-slide"+ index+1 +"'><img src='{{ asset('upload/product/'.'"+ item.image +"') }}' alt='' ></a></div></div>";
                             }
                         }
                     },
