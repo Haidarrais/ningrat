5a7df75d1a9f71e2fc4e4dc0772f043d2890def7
Haidar Rais <abdillah.fams@gmail.com>
2021-10-12 00:09:48 +0700

    cart

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -259,11 +259,11 @@
                             if (index == 0) {
                                 htmlC = `<div class="tab-pane fade show active" id="single-slide${index+1}" role="tabpanel" aria-labelledby="single-slide-tab-${index+1}">
                                             <div class="single-product-img">
-                                                <img src="{{ asset('upload/product/'. ${element.image}) }}" alt="tidak ada gambar">
+                                                <img src="{{ asset('upload/product/'. "${element.image}") }}" alt="tidak ada gambar">
                                             </div>
                                         </div>`
                                 htmlL = `<div class="single-tab-menu">
-                                            <a class="active" data-toggle="tab" id="single-slide-tab-${index+1}"" href="#single-slide${index+1}""><img src="{{ asset('upload/product/'. ${element.image}) }}" alt="" ></a>
+                                            <a class="active" data-toggle="tab" id="single-slide-tab-${index+1}"" href="#single-slide${index+1}""><img src="{{ asset('upload/product/'. "${element.image}") }}" alt="" ></a>
                                         </div>`
                             }
                         });
