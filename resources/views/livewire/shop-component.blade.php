6ce13733d676991d6dc7b6ae19fa260e09c698a7
Haidar Rais <abdillah.fams@gmail.com>
2021-10-12 01:22:29 +0700

    cart

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -261,7 +261,7 @@
                                                 <img src="{{ asset('upload/product/${value.image}') }}" alt="tidak ada gambar">
                                             </div>
                                         </div>`
-                                htmlL += `<div class="single-tab-menu" style="width:193px;">
+                                htmlL += `<div class="single-tab-menu" style="width:193px;display:flex;">
                                             <a class="active" data-toggle="tab" id="single-slide-tab-${key+1}${id}" href="#single-slide${key+1}${id}"><img src="{{ asset('upload/product/${value.image}') }}" alt="" ></a>
                                         </div>`
                             }else{
@@ -270,7 +270,7 @@
                                                 <img src="{{ asset('upload/product/${value.image}') }}" alt="tidak ada gambar">
                                             </div>
                                         </div>`
-                                htmlL += `<div class="single-tab-menu" style="width:193px;">
+                                htmlL += `<div class="single-tab-menu" style="width:193px;display:flex;">
                                             <a data-toggle="tab" id="single-slide-tab-${key+1}${id}" href="#single-slide${key+1}${id}"><img src="{{ asset('upload/product/${value.image}') }}" alt="" ></a>
                                         </div>`
                             }
