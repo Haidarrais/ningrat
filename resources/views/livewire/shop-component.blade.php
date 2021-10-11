7ad1f71022fc532bf48cd40708ad74974c74c399
Haidar Rais <abdillah.fams@gmail.com>
2021-10-11 23:54:56 +0700

    cart

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -262,7 +262,7 @@
                                             </div>
                                         </div>`
                                 htmlL = `<div class="single-tab-menu">
-                                            <a class="active" data-toggle="tab" id="single-slide-tab-${index+1}"" href="#single-slide${index+1}""><img src="{{ asset('upload/product/'. ${element.image}") }}" alt="" ></a>
+                                            <a class="active" data-toggle="tab" id="single-slide-tab-${index+1}"" href="#single-slide${index+1}""><img src="{{ asset('upload/product/'. ${element.image}) }}" alt="" ></a>
                                         </div>`
                             }
                         });
