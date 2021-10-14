4497b39e8acfc3a160b37b6417a253668a6993f1
Haidar Rais <abdillah.fams@gmail.com>
2021-10-14 21:07:41 +0700

    shop

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -75,7 +75,7 @@
                                                             <li>
                                                                 <a href="#"><i class="icon icon-Restart"></i></a>
                                                             </li>
-                                                            <li><a href="{{ asset('upload/product/' . $img) }}" data-toggle="modal" data-target=".productModal{{$stock->id}}" onclick="setIndex({{$stock->product_id}})"><i class="icon icon-Search"></i></a></li>
+                                                            <li><a href="{{ asset('upload/product/' . $img) }}" data-toggle="modal" data-target=".productModal{{$stock->id}}"><i class="icon icon-Search"></i></a></li>
                                                         </ul>
                                                         <a type="button" href="#" class="p-cart-btn default-btn" wire:click="store({{$stock->id}}, '{{$stock->product->name}}' , {{$price}})">Add to cart</a>
                                                     </div>
@@ -131,7 +131,7 @@
                                                     <li>
                                                         <a href="#"><i class="icon icon-Restart"></i></a>
                                                     </li>
-                                                    <li><a href="{{ asset('upload/product/' . $img) }}" data-toggle="modal" data-target=".productModal{{$stock->id}}" onclick="setIndex({{$stock->product_id}})"><i class="icon icon-Search"></i></a></li>
+                                                    <li><a href="{{ asset('upload/product/' . $img) }}" data-toggle="modal" data-target=".productModal{{$stock->id}}"><i class="icon icon-Search"></i></a></li>
                                                 </ul>
                                             </div>
                                         </div>
@@ -247,8 +247,9 @@
             </div>
         @endforeach
         <!-- END QUICKVIEW PRODUCT -->
+        @foreach ($stocks as $item)
         <script>
-            function setIndex(id) {
+                let id = <?php echo $item->product_id ?>
                 // index = id;
                 // console.log(index);
                 var url = "{{route('picture.show', ":id")}}";
@@ -285,8 +286,7 @@
                         $(`#myTabContent${id}`).html(htmlC)
                     }
                 });
-
-            };
         </script>
+        @endforeach
 </div>
 
