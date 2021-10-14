fb97e34d94e7858b2f7d507535942d5a3dd836ab
Haidar Rais <abdillah.fams@gmail.com>
2021-10-14 21:35:51 +0700

    shop

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -221,8 +221,8 @@
         <script src="{{ asset('assets/js/plugins.js') }}"></script>
         <script src="{{ asset('assets/js/ajax-mail.js') }}"></script>
         <link rel="stylesheet" href="{{ asset('vendor/select2/css/select2.min.css') }}">
+        <script src="{{ asset('assets/js/main.js') }}"></script>
         @yield('modal')
         <x-livewire-alert::scripts />
-        <script src="{{ asset('assets/js/main.js') }}"></script>
     </body>
 </html>
