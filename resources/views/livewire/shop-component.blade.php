a304b10c3bf6149b4e30257a3889b8b326ed6967
Haidar Rais <abdillah.fams@gmail.com>
2021-10-14 20:53:10 +0700

    shop

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -283,38 +283,40 @@
                         });
                         $(`#myTabList${id}`).html(htmlL)
                         $(`#myTabContent${id}`).html(htmlC)
-
                         $(`#myTabList${id}`).slick({settings: {unslick :1}});
-                        $(`#myTabList${id}`).slick({
-                            dots: false,
-                            arrows: false,
-                            slidesToShow: 4,
-                            responsive: [
-                                {
-                                    breakpoint: 1200,
-                                    settings: {
-                                        slidesToShow: 3,
-                                        slidesToScroll: 3
-                                    }
-                                },
-                                {
-                                    breakpoint: 991,
-                                    settings: {
-                                        slidesToShow: 3,
-                                        slidesToScroll: 2
-                                    }
-                                },
-                                {
-                                    breakpoint: 480,
-                                    settings: {
-                                        slidesToShow: 3,
-                                        slidesToScroll: 3
-                                    }
-                                }
-                            ]
-                        }).delay( 1000 );
+                        $(`#myTabList${id}`).hide();
+
                         $('.modal').on('shown.bs.modal', function (e) {
                             $(`#myTabList${id}`).resize();
+                            $(`#myTabList${id}`).show();
+                            $(`#myTabList${id}`).slick({
+                                dots: false,
+                                arrows: false,
+                                slidesToShow: 4,
+                                responsive: [
+                                    {
+                                        breakpoint: 1200,
+                                        settings: {
+                                            slidesToShow: 3,
+                                            slidesToScroll: 3
+                                        }
+                                    },
+                                    {
+                                        breakpoint: 991,
+                                        settings: {
+                                            slidesToShow: 3,
+                                            slidesToScroll: 2
+                                        }
+                                    },
+                                    {
+                                        breakpoint: 480,
+                                        settings: {
+                                            slidesToShow: 3,
+                                            slidesToScroll: 3
+                                        }
+                                    }
+                                ]
+                            });
                         });
                         $('.single-slide-menu a').on('click',function(e){
                             e.preventDefault();
