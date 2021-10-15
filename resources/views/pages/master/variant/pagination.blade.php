2ce0044fc6d5bf2ac985b29f5430a4540469709f
Haidar Rais <abdillah.fams@gmail.com>
2021-10-15 20:59:27 +0700

    badge

--- resources/views/pages/master/variant/pagination.blade.php
+++ resources/views/pages/master/variant/pagination.blade.php
@@ -4,6 +4,7 @@
             <th scope="col">#</th>
             <th scope="col">Nama</th>
             <th scope="col">Kategori</th>
+            <th scope="col">Status</th>
             <th scope="col">Aksi</th>
         </tr>
     </thead>
@@ -13,6 +14,13 @@
                 <th scope="row">{{ ($variants->currentpage()-1) * $variants->perpage() + $loop->index + 1 }}</th>
                 <td>{{ $value->name??"unset" }}</td>
                 <td>{{$value->category->name??"unset"}}</td>
+                <td>
+                    @if ($value->id == $value->parent_id)
+                    <span class="badge badge-primary">Varian Utama</span>
+                    @else
+                    <span class="badge badge-secondary">Sub Varian dari : {{$value->subCategory->name ?? "unset"}}</span>
+                    @endif
+                </td>
                 <td scope="row">
                     <button type="button" class="btn btn-sm btn-success" onclick="editData({{ $value->id }})">Edit</button>
                     <button class="btn btn-sm btn-danger hapus" onclick="deleteData({{ $value->id }})" type="button">Delete</button>
