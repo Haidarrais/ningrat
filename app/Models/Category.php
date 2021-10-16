1a417597a6e190ea713bcb05de73a2043df828ff
Haidar Rais <abdillah.fams@gmail.com>
2021-10-16 16:01:38 +0700

    slick

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -29,7 +29,8 @@ class Category extends Model
     }
     public function subCategory()
     {
-        return $this->belongsTo(Category::class, 'parent_id', 'id');
+        $subCategory = Category::where('parent_id', $this->id)
+        return $subCategory;
     }
     public function discount(){
         return $this->hasOne(CategoryDiscount::class);
