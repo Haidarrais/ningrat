73b7a16c95ac283078029d0ae1bf25243f769166
Haidar Rais <abdillah.fams@gmail.com>
2021-10-16 07:02:50 +0700

    all

--- resources/views/livewire/shop-component.blade.php
+++ resources/views/livewire/shop-component.blade.php
@@ -282,10 +282,10 @@
                         });
                         $(`#myTabList${id}`).html(htmlL)
                         $(`#myTabContent${id}`).html(htmlC)
-                        $('.single-slide-menu').slick({
+                        $(`#myTabList${id}`).slick({
                             settings:"unslick"
                         });
-                        $('.single-slide-menu').slick({
+                        $(`#myTabList${id}`).slick({
                             dots: false,
                             arrows: false,
                             slidesToShow: 4,
@@ -314,7 +314,7 @@
                             ]
                         });
                         $('.modal').on('shown.bs.modal', function (e) {
-                            $('.single-slide-menu').resize();
+                            $(`#myTabList${id}`).resize();
                         })
                         $('.single-slide-menu a').on('click',function(e){
                             e.preventDefault();
