7914a11545649e1c742590388248309ed34fce39
Haidar Rais <abdillah.fams@gmail.com>
2021-10-19 23:38:22 +0700

    validasi member kosong

--- resources/views/livewire/member-component.blade.php
+++ resources/views/livewire/member-component.blade.php
@@ -107,11 +107,9 @@
     @endphp
     @endif
     <script>
-    if (window.jQuery) {
         console.log($('#member_row').is(':empty'));
         if ($('#member_row').is(':empty')){
             $('#member_row').html("<h1>Member di area ini belum ada</h1>")
         }
-    }
     </script>
 </div>
