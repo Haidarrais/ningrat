686c6eca6c710c4eccff0982928369bd2b6c22eb
Haidar Rais <abdillah.fams@gmail.com>
2021-10-20 01:43:12 +0700

    validasi member kosong

--- app/Http/Livewire/CheckoutReseller.php
+++ app/Http/Livewire/CheckoutReseller.php
@@ -84,38 +84,37 @@ class CheckoutReseller extends Component
         }else{
             $kurir = $this->othercourier;
         }
-            if ($this->discountOn) {
-                if ($this->discount) {
-                    $userId = Auth::id();
-                    $discount = MasterDiscount::find($this->discount->id);
-                    $userList = json_decode($discount->userList);
-                    if ($userList == null) {
-                        $discount->userList = json_encode([$userId]);
-                    }else{
-                        foreach ($userList as $value) {
-                            if ($value == $userId) {
-                                $this->alert('error', 'gagal user sudah menggunakan diskon ini', [
-                                    'position' =>  'center',
-                                    'timer' =>  3000,
-                                    'toast' =>  true,
-                                    'text' =>  '',
-                                    'confirmButtonText' =>  'Ok',
-                                    'cancelButtonText' =>  'Cancel',
-                                    'showCancelButton' =>  false,
-                                    'showConfirmButton' =>  false,
-                                ]);
-                            }
+        if ($this->discountOn) {
+            if ($this->discount) {
+                $userId = Auth::id();
+                $discount = MasterDiscount::find($this->discount->id);
+                $userList = json_decode($discount->userList);
+                if ($userList == null) {
+                    $discount->userList = json_encode([$userId]);
+                }else{
+                    foreach ($userList as $value) {
+                        if ($value == $userId) {
+                            $this->alert('error', 'gagal user sudah menggunakan diskon ini', [
+                                'position' =>  'center',
+                                'timer' =>  3000,
+                                'toast' =>  true,
+                                'text' =>  '',
+                                'confirmButtonText' =>  'Ok',
+                                'cancelButtonText' =>  'Cancel',
+                                'showCancelButton' =>  false,
+                                'showConfirmButton' =>  false,
+                            ]);
                         }
-                        array_push($userList, $userId);
-                        $discount->update(['userList' => $userList]);
                     }
-                    $discount->save();
-
-                    return response()->json([
-                        'status' => 1,
-                        'data'  => $discount
-                    ]);
+                    array_push($userList, $userId);
+                    $discount->update(['userList' => $userList]);
                 }
+                $discount->save();
+                return response()->json([
+                    'status' => 1,
+                    'data'  => $discount
+                ]);
+            }
             $subtotal = $this->subtotal+$this->ongkir;
         }else{
             $subtotal = Cart::subtotal(2,'.','')+$this->ongkir;
