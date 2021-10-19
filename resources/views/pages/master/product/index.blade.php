edc97133a7b5b0d31b656e5b16834dfbd54a58db
Kacong Arip <arifsaqa@gmail.com>
2021-10-20 00:32:05 +0700

    asas

--- resources/views/pages/master/product/index.blade.php
+++ resources/views/pages/master/product/index.blade.php
@@ -322,6 +322,7 @@
                         category = product.category;
                         if (category.id !== category.parent_id) {
                            $("#selectSubKategori").val(category.id);
+                           console.log(`${category.parent_id}, true`);
                             $("#selectKategori").val(`${category.parent_id}, true`);    
                             }else{
                                 $("#selectKategori").val(category.id);
