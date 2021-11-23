f1d1a437dd32ade281354b5515a06659b287a704
Haidarrais <Haidarrais>
2021-11-23 15:38:56 +0700

    background

--- resources/views/livewire/checkout-component.blade.php
+++ resources/views/livewire/checkout-component.blade.php
@@ -182,10 +182,11 @@
                                                         </td>
                                                     </tr>
                                                     @php
+                                                        dd($cartItem->options->stock->user->member->subdistrict_id);
                                                         $a = $a + ($cartItem->options->stock->product->weight*$cartItem->qty);
                                                         $this->berat = $a;
-                                                        $this->sellerlocation = ($cartItem->options->stock->user->member->subdistrict_id);
-                                                        $this->sellerid = ($cartItem->options->stock->user->id);
+                                                        $this->sellerlocation = $cartItem->options->stock->user->member->subdistrict_id;
+                                                        $this->sellerid = $cartItem->options->stock->user->id;
                                                     @endphp
                                                 @endforeach
                                             @else
