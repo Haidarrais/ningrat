e6ed69227cd137bcec956a58c95ecb46266f73e1
Haidarrais <Haidarrais>
2021-11-23 14:30:05 +0700

    background

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -1,6 +1,6 @@
 <div>
         <!-- Hero Area Start -->
-        <div class="ht-hero-section fix" wire:ignore>
+        {{-- <div class="ht-hero-section fix" wire:ignore>
             <div class="ht-hero-slider">
                 @foreach ($carousel as $crsl)
                     <div class="ht-single-slide">
@@ -10,7 +10,29 @@
                     </div>
                 @endforeach
             </div>
-        </div>
+        </div> --}}
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                @foreach ($carousel as $key => $crsl)
+                    <li data-target="#carouselExampleIndicators" data-slide-to="{{$key}}" @if($key==0)class="active"@endif></li>
+                @endforeach
+                </ol>
+                <div class="carousel-inner">
+                    @foreach ($carousel as $key => $crsl)
+                        <div class="carousel-item @if($key==0)active @endif">
+                            <img class="d-block w-100" src="{{ asset('uploads/contents/'. $crsl->image) }}" alt="$crsl->image">
+                        </div>
+                    @endforeach
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+          </div>
         <!-- Hero Area End -->
         {{-- <!-- Food Categry Area Start -->
         <div class="food-category-area pt-105 pb-70">
