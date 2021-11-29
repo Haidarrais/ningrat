3a6ca3ee66e87e9265246fd11704f07af4b98554
Haidarrais <Haidarrais>
2021-11-29 15:52:18 +0700

    invoice

--- app/Http/Controllers/Web/TransactionController.php
+++ app/Http/Controllers/Web/TransactionController.php
@@ -284,6 +284,7 @@ class TransactionController extends Controller
             $data = (new InvoiceItem())->title($detailOrder->stock->product->name)->pricePerUnit($detailOrder->stock->product->price);
             array_push($items, $data);
         }
+        dd($detailOrder);
 
         $notes = [
             'your multiline',
