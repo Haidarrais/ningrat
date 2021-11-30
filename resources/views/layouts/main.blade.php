7ff15750990895487ff9367884b4ffd5e80a6809
Kacong Arip <arifsaqa@gmail.com>
2021-12-01 00:59:42 +0700

    title n detail order bug fixed!

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,139 +1,149 @@
 <!doctype html>
 <html class="no-js" lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Naturecircle - Premium eCommerce Template</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Favicon -->
-        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.png') }}">
-        @livewireStyles
-        <!-- All css here -->
-        @yield('css')
-        <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/ie7.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/plugins.css') }}">
-        <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
-        <link rel="stylesheet" href="https://rawgit.com/kenwheeler/slick/master/slick/slick.css">
-        <link rel="stylesheet" href="https://rawgit.com/kenwheeler/slick/master/slick/slick-theme.css">
 
-        <script src="{{ asset('assets/js/vendor/modernizr-3.5.0.min.js') }}"></script>
-    </head>
-    <body>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Ningrat Official - Memenuhi kebutuhan anda dengan solusi yang terbaik</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicon.png') }}">
+    @livewireStyles
+    <!-- All css here -->
+    @yield('css')
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/ie7.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/plugins.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="https://rawgit.com/kenwheeler/slick/master/slick/slick.css">
+    <link rel="stylesheet" href="https://rawgit.com/kenwheeler/slick/master/slick/slick-theme.css">
 
-        <livewire:header-component />
-        {{-- @include('partials.main.hero')
+    <script src="{{ asset('assets/js/vendor/modernizr-3.5.0.min.js') }}"></script>
+</head>
+
+<body>
+
+    <livewire:header-component />
+    {{-- @include('partials.main.hero')
         @include('partials.main.category')
         @include('partials.main.product')
         @include('partials.main.banner')
         @include('partials.main.featured')
         @include('partials.main.testimonial')
         @include('partials.main.quickview') --}}
-        {{$slot ?? ''}}
-        @yield('content')
+    {{$slot ?? ''}}
+    @yield('content')
 
-        @include('partials.main.footer')
-        <!-- All js here -->
-        <style>
-            .map-responsive{
-                overflow:hidden;
-                padding-bottom:50%;
-                position:relative;
-                height:0;
-            }
-            .map-responsive iframe{
-                left:0;
-                top:0;
-                height:100%;
-                width:100%;
-                position:absolute;
-            }
-            button:disabled {
-                cursor: not-allowed;
-                pointer-events: all !important;
-            }
-            ul.timeline {
-                list-style-type: none;
-                position: relative;
-            }
-            ul.timeline:before {
-                content: ' ';
-                background: #d4d9df;
-                display: inline-block;
-                position: absolute;
-                left: 29px;
-                width: 2px;
-                height: 100%;
-                z-index: 400;
-            }
-            ul.timeline > li {
-                margin: 20px 0;
-                padding-left: 50px;
-            }
-            ul.timeline > li:before {
-                content: ' ';
-                background: white;
-                display: inline-block;
-                position: absolute;
-                border-radius: 50%;
-                border: 3px solid green;
-                left: 20px;
-                width: 20px;
-                height: 20px;
-                z-index: 400;
-            }
-            .rating {
-                display: flex;
-                flex-direction: row-reverse;
-                justify-content: center
-            }
+    @include('partials.main.footer')
+    <!-- All js here -->
+    <style>
+        .map-responsive {
+            overflow: hidden;
+            padding-bottom: 50%;
+            position: relative;
+            height: 0;
+        }
 
-            .rating>input {
-                display: none
-            }
+        .map-responsive iframe {
+            left: 0;
+            top: 0;
+            height: 100%;
+            width: 100%;
+            position: absolute;
+        }
 
-            .rating>label {
-                position: relative;
-                width: 1em;
-                font-size: 2vw;
-                color: #FFD600;
-                cursor: pointer
-            }
+        button:disabled {
+            cursor: not-allowed;
+            pointer-events: all !important;
+        }
 
-            .rating>label::before {
-                content: "\2605";
-                position: absolute;
-                opacity: 0
-            }
+        ul.timeline {
+            list-style-type: none;
+            position: relative;
+        }
 
-            .rating>label:hover:before,
-            .rating>label:hover~label:before {
-                opacity: 1 !important
-            }
+        ul.timeline:before {
+            content: ' ';
+            background: #d4d9df;
+            display: inline-block;
+            position: absolute;
+            left: 29px;
+            width: 2px;
+            height: 100%;
+            z-index: 400;
+        }
 
-            .rating>input:checked~label:before {
-                opacity: 1
-            }
+        ul.timeline>li {
+            margin: 20px 0;
+            padding-left: 50px;
+        }
 
-            .rating:hover>input:checked~label:before {
-                opacity: 0.4
-            }
-            .switch {
+        ul.timeline>li:before {
+            content: ' ';
+            background: white;
+            display: inline-block;
+            position: absolute;
+            border-radius: 50%;
+            border: 3px solid green;
+            left: 20px;
+            width: 20px;
+            height: 20px;
+            z-index: 400;
+        }
+
+        .rating {
+            display: flex;
+            flex-direction: row-reverse;
+            justify-content: center
+        }
+
+        .rating>input {
+            display: none
+        }
+
+        .rating>label {
+            position: relative;
+            width: 1em;
+            font-size: 2vw;
+            color: #FFD600;
+            cursor: pointer
+        }
+
+        .rating>label::before {
+            content: "\2605";
+            position: absolute;
+            opacity: 0
+        }
+
+        .rating>label:hover:before,
+        .rating>label:hover~label:before {
+            opacity: 1 !important
+        }
+
+        .rating>input:checked~label:before {
+            opacity: 1
+        }
+
+        .rating:hover>input:checked~label:before {
+            opacity: 0.4
+        }
+
+        .switch {
             position: relative;
             display: inline-block;
             width: 60px;
             height: 34px;
-            }
+        }
 
-            .switch input {
+        .switch input {
             opacity: 0;
             width: 0;
             height: 0;
-            }
+        }
 
-            .slider {
+        .slider {
             position: absolute;
             cursor: pointer;
             top: 0;
@@ -143,9 +153,9 @@
             background-color: #ccc;
             -webkit-transition: .4s;
             transition: .4s;
-            }
+        }
 
-            .slider:before {
+        .slider:before {
             position: absolute;
             content: "";
             height: 26px;
@@ -155,47 +165,51 @@
             background-color: white;
             -webkit-transition: .4s;
             transition: .4s;
-            }
+        }
 
-            input:checked + .slider {
+        input:checked+.slider {
             background-color: #2196F3;
-            }
+        }
 
-            input:focus + .slider {
+        input:focus+.slider {
             box-shadow: 0 0 1px #2196F3;
-            }
+        }
 
-            input:checked + .slider:before {
+        input:checked+.slider:before {
             -webkit-transform: translateX(26px);
             -ms-transform: translateX(26px);
             transform: translateX(26px);
-            }
+        }
 
-            /* Rounded sliders */
-            .slider.round {
+        /* Rounded sliders */
+        .slider.round {
             border-radius: 34px;
-            }
+        }
 
-            .slider.round:before {
+        .slider.round:before {
             border-radius: 50%;
-            }
-            .lds-ripple {
+        }
+
+        .lds-ripple {
             display: inline-block;
             position: relative;
             width: 80px;
             height: 80px;
-            }
-            .lds-ripple div {
+        }
+
+        .lds-ripple div {
             position: absolute;
             border: 4px solid #fff;
             opacity: 1;
             border-radius: 50%;
             animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
-            }
-            .lds-ripple div:nth-child(2) {
+        }
+
+        .lds-ripple div:nth-child(2) {
             animation-delay: -0.5s;
-            }
-            @keyframes lds-ripple {
+        }
+
+        @keyframes lds-ripple {
             0% {
                 top: 36px;
                 left: 36px;
@@ -203,6 +217,7 @@
                 height: 0;
                 opacity: 1;
             }
+
             100% {
                 top: 0px;
                 left: 0px;
@@ -210,24 +225,26 @@
                 height: 72px;
                 opacity: 0;
             }
-            }
-        </style>
+        }
+    </style>
 
 
-        @livewireScripts
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10">
-        </script>
-        <script src="{{ asset('assets/js/vendor/jquery-3.2.1.min.js') }}"></script>
-        <script src="{{ asset('assets/js/popper.min.js') }}"></script>
-        <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-        <script src="{{ asset('assets/js/plugins.js') }}"></script>
-        <script src="https://rawgit.com/kenwheeler/slick/master/slick/slick.js">
-        <script src="{{ asset('assets/js/ajax-mail.js') }}"></script>
-        <link rel="stylesheet" href="{{ asset('vendor/select2/css/select2.min.css') }}">
-        @yield('script')
-        <script src="{{ asset('assets/js/main.js') }}"></script>
-        @stack('modal')
-        @yield('modal')
-        <x-livewire-alert::scripts />
-    </body>
+    @livewireScripts
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10">
+    </script>
+    <script src="{{ asset('assets/js/vendor/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins.js') }}"></script>
+    <script src="https://rawgit.com/kenwheeler/slick/master/slick/slick.js">
+        < script src = "{{ asset('assets/js/ajax-mail.js') }}" >
+    </script>
+    <link rel="stylesheet" href="{{ asset('vendor/select2/css/select2.min.css') }}">
+    @yield('script')
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+    @stack('modal')
+    @yield('modal')
+    <x-livewire-alert::scripts />
+</body>
+
 </html>
