5686bbd86b736df33d253ab935898ee54372e56d
Haidarrais <abdillah.fams@gmail.com>
2021-12-25 16:55:50 +0700

    blog

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -66,7 +66,7 @@
                     @foreach ($banner as $bnr)
                         <div class="col-md-6">
                             <div class="shop-banner-img">
-                                <a href="{{ route('show.blog', ['id'=>$bnr->id]) }}"><img src="{{ asset('uploads/contents/'. $bnr->image) }}" alt="Banner"></a>
+                                <a><img src="{{ asset('uploads/contents/'. $bnr->image) }}" alt="Banner" wire:click="goBlog({{$bnr->id}})"></a>
                             </div>
                         </div>
                     @endforeach
