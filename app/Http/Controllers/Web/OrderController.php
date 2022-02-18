684d451dee9e6d6c0473a76eae37fc0e935cd713
Kacong Arip <arifsaqa@gmail.com>
2022-02-18 14:28:55 +0700

    oke

--- app/Http/Controllers/Web/OrderController.php
+++ app/Http/Controllers/Web/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Web;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\MasterDiscount;
 use App\Models\Order;
 use App\Models\OrderDetail;
@@ -37,36 +38,36 @@ class OrderController extends Controller
         $bawahan = User::where('upper', $user->id)->get()->pluck('id')->toArray();
         $query = Order::query();
         $query->with('details');
-        if($role == 'superadmin') {
+        if ($role == 'superadmin') {
         } else {
-            $query->where(function($q) use($bawahan, $user) {
+            $query->where(function ($q) use ($bawahan, $user) {
                 $q->whereIn('user_id', $bawahan)
                     ->orWhere('user_id', $user->id);
             });
         }
-        $query->when($keyword, function($q) use($keyword) {
-            if($keyword == "Pending") {
+        $query->when($keyword, function ($q) use ($keyword) {
+            if ($keyword == "Pending") {
                 $keyword = 0;
-            } else if($keyword == "Dikemas") {
+            } else if ($keyword == "Dikemas") {
                 $keyword = 1;
-            } else if($keyword == "Dikirim") {
+            } else if ($keyword == "Dikirim") {
                 $keyword = 2;
-            } else if($keyword == "Diterima") {
+            } else if ($keyword == "Diterima") {
                 $keyword = 3;
-            } else if($keyword == "Selesai") {
+            } else if ($keyword == "Selesai") {
                 $keyword = 4;
-            } else if($keyword == "Ditolak") {
+            } else if ($keyword == "Ditolak") {
                 $keyword = 5;
-            } else if($keyword == "Batal") {
+            } else if ($keyword == "Batal") {
                 $keyword = 6;
             }
             $q->where('member_name', "LIKE", "%$keyword%")
-            ->orWhere('member_address', 'LIKE', "%$keyword%")
-            ->orWhere('status', 'LIKE', "%$keyword%");
+                ->orWhere('member_address', 'LIKE', "%$keyword%")
+                ->orWhere('status', 'LIKE', "%$keyword%");
         });
         $orders = $query->latest()->paginate(10);
 
-        if($request->ajax()) {
+        if ($request->ajax()) {
             return view('pages.order.order.pagination', compact('orders', 'data'))->render();
         }
         return view('pages.order.order.index', compact('orders', 'data'));
@@ -88,21 +89,21 @@ class OrderController extends Controller
         $discount_role_based = 0;
         //get minimal order
         $setting_role = "";
-        if($role == 'superadmin') {
-        } else if($role == 'distributor') {
+        if ($role == 'superadmin') {
+        } else if ($role == 'distributor') {
             $minimal_transaction = $user_updated_at > 2020 ?
-                Setting::where('role', 'new-distributor')->first()->minimal_transaction??0 :
-                Setting::where('role', 'old-distributor')->first()->minimal_transaction??0;
-                 $discount_role_based= Setting::where('role', 'old-distributor')->first()->discount??0;
-            $monthly_min_transaction = Setting::where('role', 'old-distributor')->first()->value??0;
+                Setting::where('role', 'new-distributor')->first()->minimal_transaction ?? 0 :
+                Setting::where('role', 'old-distributor')->first()->minimal_transaction ?? 0;
+            $discount_role_based = Setting::where('role', 'old-distributor')->first()->discount ?? 0;
+            $monthly_min_transaction = Setting::where('role', 'old-distributor')->first()->value ?? 0;
             $products =
-            $products = Stock::with(['product', 'user'])->where('user_id', 0)->where('status', 1)->where('stock', '>', 0)->get()??[];
+                $products = Stock::with(['product', 'user'])->where('user_id', 0)->where('status', 1)->where('stock', '>', 0)->get() ?? [];
             // dd($products);
-            $setting_role = $user_updated_at > 2020? "new-distributor": "old-distributor";
+            $setting_role = $user_updated_at > 2020 ? "new-distributor" : "old-distributor";
         } else {
             $setting_role = $role;
             $minimal_transaction = Setting::where('role', $role)->first()->minimal_transaction ?? 0;
-            $monthly_min_transaction = Setting::where('role', $role)->first()->value??0;
+            $monthly_min_transaction = Setting::where('role', $role)->first()->value ?? 0;
             $discount_role_based = Setting::where('role', $role)->first()->discount ?? 0;
             $products = Stock::with(['product', 'user'])->where('user_id', $user->upper)->where('status', 1)->where('stock', '>', 0)->get();
         }
@@ -111,11 +112,11 @@ class OrderController extends Controller
         } else {
             $hirarki = User::where('upper', $user->id)->get()->pluck('id')->toArray();
         }
-        $this_month_total_transaction = $this->getMonthTotalTransaction($hirarki,$user,$monthly_min_transaction);
+        $this_month_total_transaction = $this->getMonthTotalTransaction($hirarki, $user, $monthly_min_transaction);
         // dd($this_month_total_transaction);
         $upper_origin = [];
         $upper = User::with('member')->find($user->upper);
-        if($upper) {
+        if ($upper) {
             $upper_origin = [
                 'city' => $upper->member->city_id,
                 'subdistrict' => $upper->member->subdistrict_id
@@ -123,12 +124,12 @@ class OrderController extends Controller
         }
         // dd($products);
         if ($request->ajax()) {
-            return $role=="distributor"?view('pages.order.order.paginationdistributor', compact('products', 'upper_origin', 'discount', 'minimal_transaction', 'role', 'this_month_total_transaction', 'monthly_min_transaction', 'discount_role_based', 'setting_role'))->render(): view('pages.order.order.pagination_create_order', compact('products', 'upper_origin', 'discount', 'minimal_transaction', 'role', 'this_month_total_transaction', 'monthly_min_transaction', 'discount_role_based', 'setting_role'))->render();
-        }else{
-            if($role == 'distributor') {
-                return view('pages.order.order.distributor-page', compact('products', 'upper_origin', 'minimal_transaction', 'role','this_month_total_transaction','monthly_min_transaction', 'discount_role_based', 'setting_role'));
+            return $role == "distributor" ? view('pages.order.order.paginationdistributor', compact('products', 'upper_origin', 'discount', 'minimal_transaction', 'role', 'this_month_total_transaction', 'monthly_min_transaction', 'discount_role_based', 'setting_role'))->render() : view('pages.order.order.pagination_create_order', compact('products', 'upper_origin', 'discount', 'minimal_transaction', 'role', 'this_month_total_transaction', 'monthly_min_transaction', 'discount_role_based', 'setting_role'))->render();
+        } else {
+            if ($role == 'distributor') {
+                return view('pages.order.order.distributor-page', compact('products', 'upper_origin', 'minimal_transaction', 'role', 'this_month_total_transaction', 'monthly_min_transaction', 'discount_role_based', 'setting_role'));
             }
-            return view('pages.order.order.order-page', compact('products', 'upper_origin', 'discount', 'minimal_transaction', 'role','this_month_total_transaction','monthly_min_transaction', 'discount_role_based', 'setting_role'));
+            return view('pages.order.order.order-page', compact('products', 'upper_origin', 'discount', 'minimal_transaction', 'role', 'this_month_total_transaction', 'monthly_min_transaction', 'discount_role_based', 'setting_role'));
         }
     }
 
@@ -149,7 +150,7 @@ class OrderController extends Controller
             //     'data'=>$data,
             //     'data[]id'=>$data['id']
             // ]);
-            if(!$user->member->city_id) {
+            if (!$user->member->city_id) {
                 return response()->json([
                     'status' => true,
                     'message' => [
@@ -159,7 +160,7 @@ class OrderController extends Controller
                 ], 500);
             }
             $price = 0;
-            $invoice = $this->generateInvoice('INV', date('Ymd').substr(str_shuffle($this->numeric), 0, 12));
+            $invoice = $this->generateInvoice('INV', date('Ymd') . substr(str_shuffle($this->numeric), 0, 12));
             $order = Order::create([
                 'user_id' => $user->id,
                 'city_id' => $user->member->city_id,
@@ -169,14 +170,14 @@ class OrderController extends Controller
                 'member_address' => $user->member->address,
                 'subtotal' => 0,
                 'cost' => $data['cost'],
-                'discount'=>$data['discount'],
-                'subsidy_cost'=>$data['ongkir-discount'],
+                'discount' => $data['discount'],
+                'subsidy_cost' => $data['ongkir-discount'],
                 'shipping' => $data['courier'],
                 'status' => $data['status'] ?? 0
             ]);
             $order_details = [];
             foreach ($data['id'] as $key => $value) {
-                if($data['qty'][$key] == 0) continue;
+                if ($data['qty'][$key] == 0) continue;
                 $product = Product::find($value);
                 $temp_data = [];
                 $temp_data['order_id'] = $order->id;
@@ -185,8 +186,8 @@ class OrderController extends Controller
                 $temp_data['price'] = $data['price'][$key];
                 $temp_data['weight'] = $product->weight;
                 $temp_data['qty'] = $data['qty'][$key];
-//                $temp_data['created_at'] = now();
-//                $temp_data['updated_at'] = now();
+                //                $temp_data['created_at'] = now();
+                //                $temp_data['updated_at'] = now();
                 // $price += $product->price * $data['qty'][$key];
                 $price += $data['price'][$key];
                 array_push($order_details, $temp_data);
@@ -196,9 +197,9 @@ class OrderController extends Controller
                 'subtotal' => $price
             ]);
             // Jika Reseller
-            if(isset($data['status'])) {
+            if (isset($data['status'])) {
                 foreach ($data['id'] as $key => $value) {
-                    if($data['qty'][$key] == 0) continue;
+                    if ($data['qty'][$key] == 0) continue;
                     $product = Product::find($value);
                     $stock_reseller_temp = Stock::where('product_id', $product->id)->where('user_id', $user_id)->first();
                     $stock_now = $stock_reseller_temp->stock - $data['qty'][$key];
@@ -216,7 +217,7 @@ class OrderController extends Controller
                 ],
                 'redirect' => route('order.index')
             ], 200);
-        } catch(\Exception $e) {
+        } catch (\Exception $e) {
             return response()->json([
                 'status' => true,
                 'message' => [
@@ -228,13 +229,14 @@ class OrderController extends Controller
         }
     }
 
-    public function store_reseller(Request $request) {
+    public function store_reseller(Request $request)
+    {
         // DB::beginTransaction();
         try {
             $user_id = Auth::user()->id;
             $user = User::with('member')->find($user_id);
             $data = $request->all();
-            if(!$user->member->city_id) {
+            if (!$user->member->city_id) {
                 return response()->json([
                     'status' => true,
                     'message' => [
@@ -244,9 +246,8 @@ class OrderController extends Controller
                 ], 500);
             }
             $price = 0;
-            $invoice = $this->generateInvoice('INV', date('Ymd').substr(str_shuffle($this->numeric), 0, 12));
-
-        } catch(\Exception $e) {
+            $invoice = $this->generateInvoice('INV', date('Ymd') . substr(str_shuffle($this->numeric), 0, 12));
+        } catch (\Exception $e) {
             return response()->json([
                 'status' => true,
                 'message' => [
@@ -273,7 +274,8 @@ class OrderController extends Controller
         ], 200);
     }
 
-    public function show_resi($id) {
+    public function show_resi($id)
+    {
         $order = Order::find($id);
         return response()->json([
             'status' => true,
@@ -281,7 +283,8 @@ class OrderController extends Controller
         ], 200);
     }
 
-    public function set_resi(Request $request, $id) {
+    public function set_resi(Request $request, $id)
+    {
         $order = Order::find($id);
         $order->update([
             'waybill' => $request->waybill
@@ -324,7 +327,7 @@ class OrderController extends Controller
                     'body' => 'Hmmm'
                 ]
             ], 200);
-        }else{
+        } else {
             $order = Order::find($id);
             $order->update([
                 'cost' => $request->cost
@@ -350,25 +353,26 @@ class OrderController extends Controller
         //
     }
 
-    public function set_status($id, $status) {
+    public function set_status($id, $status)
+    {
         $order = Order::find($id);
         $user  = User::find($order->user_id);
         $order->update([
             'status' => $status == 3 ? 4 : $status
         ]);
-        if($status == 1) {
+        if ($status == 1) {
             $text = "Dikemas";
-        } else if($status == 2) {
+        } else if ($status == 2) {
             $text = "Dikirim";
-        } else if($status == 3) {
+        } else if ($status == 3) {
             $text = "Diterima";
-        } else if($status == 4) {
+        } else if ($status == 4) {
             $text = "Selesai";
             foreach ($order->details()->get() as $key => $value) {
                 $stock = Stock::where('user_id', $order->user_id)
-                                ->where('product_id', $value->product_id)
-                                ->first();
-                if($stock) {
+                    ->where('product_id', $value->product_id)
+                    ->first();
+                if ($stock) {
                     $temp_total = $stock->stock ?? 0;
                     $stock->update([
                         'stock' => $value->qty + $temp_total
@@ -378,7 +382,7 @@ class OrderController extends Controller
                     $type = 2;
                     $user_order = User::find($order->user_id);
                     $role_user_order = $user_order->getRoleNames()->first();
-                    if($role_user_order == 'reseller') {
+                    if ($role_user_order == 'reseller') {
                         $type = 2;
                     }
                     Stock::create([
@@ -391,17 +395,30 @@ class OrderController extends Controller
                 }
             }
             $min_order = Setting::where([
-                ['key','=', 'minimal-belanja-point'],
-                ['role', '=', $user->role_name]])
+                ['key', '=', 'minimal-belanja-point'],
+                ['role', '=', $user->role_name]
+            ])
                 ->first();
-            if($order->subtotal >= (int) $min_order->value) {
-                foreach($order->details()->get() as $key => $detail) {
-                    $point = Point::where('category_id', 
-                    $detail->product->category_id)->first();
-                    if($point) {
+            if ($order->subtotal >= (int) $min_order->value) {
+                foreach ($order->details()->get() as $key => $detail) {
+
+                    $min_point = 0;
+                    $point = Point::where(
+                        'category_id',
+                        $detail->product->category_id
+                    )->first();
+                    if (!$point) {
+                        $category = Category::where('parent_id', $detail->product->category_id)->first();
+                        if ($category && $p = Point::where('category_id', $category->id)->first()) {
+                            $min_point = $p->min;
+                        }
+                    } else {
+                        $min_point = $point->min;
+                    }
+                    if ($min_point) {
                         $qty = $detail->qty;
-                        $total = $qty/$point->min;
-                        if(floor($total) > 0) {
+                        $total = $qty / $min_point;
+                        if (floor($total) > 0) {
                             DB::table("point_user")->insert([
                                 'point_id' => $point->id,
                                 'user_id' => $order->user_id,
@@ -411,25 +428,24 @@ class OrderController extends Controller
                             ]);
                         }
                     }
-                    # code...
                 }
             }
-
-        } else if($status == 5) {
+        } else if ($status == 5) {
             $text = "Ditolak";
-        } else if($status == 6) {
+        } else if ($status == 6) {
             $text = "DiBatalkan";
         }
         return response()->json([
             'status' => true,
             'message' => [
                 'head' => 'Berhasil',
-                'body' => 'Order Telah '. $text
+                'body' => 'Order Telah ' . $text
             ]
         ], 200);
     }
 
-    private function getMonthTotalTransaction($hirarki, $user, $min_trans_per_month){
+    private function getMonthTotalTransaction($hirarki, $user, $min_trans_per_month)
+    {
         $todaysMonth = Carbon::now()->month;
         $processedOrders = [];
         $orders =  Order::select(
@@ -459,10 +475,10 @@ class OrderController extends Controller
         foreach ($filteredArray as $index => $value) {
             $processedOrders[0] = $value;
         }
-        if (count($processedOrders)>0) {
-            return intval($processedOrders[0]["sums"])??0;
+        if (count($processedOrders) > 0) {
+            return intval($processedOrders[0]["sums"]) ?? 0;
             # code...
-        }else{
+        } else {
             return 0;
         }
     }
